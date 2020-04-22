de267b14728acac95d9a12d8d99f00f376d93371
kingdomwilks <46867117+kingdomwilks@users.noreply.github.com>
2020-04-22 17:57:42 +0100

    Add files via upload

--- content.js
+++ content.js
@@ -1,62 +1,317 @@
-
+const edexcelSpanish = {
+  "week": "semana",
+  "welcome": "Bienvenidos",
+  "weekend": "fin de semana",
+  "and": "y",
+  "month": "mes",
+  "sun": "sol",
+  "death": "muerte",
+  "deaths": "muertes",
+  "appetite":"apetito",
+  "Apricot":"albaricoque",
+  "Bean":"alubia",
+  "Brussels sprouts":"coles",
+  "Cafe":"cafetería",
+  "Champagne":"champán",
+  "Cherry":"cereza",
+  "Cider":"sidra",
+  "Cucumber":"pepino",
+  "Customer":"cliente",
+  "Hypermarket":"hipermercado",
+  "Inn":"posada",
+  "Jar":"tarro",
+  "Kebab":"brocheta",
+  "Margarine":"margarina",
+  "Mince":"carne",
+  "Mustard":"mostaza",
+  "Mushroom":"champiñon",
+  "Napkin":"servilleta",
+  "Place":"cubierto",
+  "Plum":"ciruela",
+  "Radish":"rábano",
+  "Day off":"día de descanso",
+  "Bread roll":"panecillo",
+  "Salad dressing":"aliño para ensalada",
+  "Slice":"rebanada",
+  "Spaghetti":"espaguetis",
+  "Tablecloth":"mantel",
+  "Vanilla":"vainilla",
+  "":"",
+  "Artichoke":"alcachofa",
+  "Beer from the tap":"cerveza de barril",
+  "Bitter":"amargo",
+  "Herbal tea":"tisana",
+  "Goose":"ganso",
+  "Leeks":"puerros",
+  "Noodles":"fideos",
+  "Raw":"crudo",
+  "Saucer":"platillo",
+  "Smoked":"ahumado",
+  "Tray":"bandeja",
+  "Turkey":"pavo",
+  "Well-cooked":"bien cocido",
+  "":"",
+  "":"",
+  "Boxer shorts":"calzoncillos",
+  "Bra":"sostén",
+  "Bracelet":"brazalete",
+  "Changing":"probador",
+  "Leggings":"leggings",
+  "Linen":"hilo",
+  "Lipstick":"lápiz",
+  "Loose":"Holgado",
+  "Nightdress":"camisón",
+  "Old-fashioned":"anticuado",
+  "Pants":"bragas",
+  "Spotted":"de lunares",
+  "Striped":"de rayas",
+  "Jumper":"jersey",
+  "Tights":"medias",
+  "Wool":"de lana",
+  "":"",
+  "Cardigan":"rebeca",
+  "Dressing gown":"bata",
+  "Dyed":"teñido",
+  "Tight":"ajustado",
+  "Velvet":"(de)terciopelo",
+  "":"",
+  "Adventurous":"Atrevido",
+  "Beautiful":"Precioso",
+  "Block of flats":"Bloque de pisos",
+  "Bossy":"autoritario",
+  "Engaged":"Prometido",
+  "Feeling":"sentimiento",
+  "Friendship":"Amistad",
+  "Goldfish":"peces de colores",
+  "Guinea pig":"cobayo",
+  "Hamster":"hámster",
+  "Loft":"ático",
+  "government": "gobierno",
+  "Multicultural":"multicultural",
+  "Pessimistic":"pesimista",
+  "Picture":"cuadro",
+  "Pet":"mascota",
+  "Survey":"encuesta",
+  "Thin":"flaco",
+  "Tidy":"ordenado",
+  "Unemployed":"desempleado",
+  "Unbearable":"insoportable",
+  "Untidy":"desordenado",
+  "":"",
+  "Acquiantance":"conocido",
+  "Adopted":"adoptado",
+  "Annoying":"irritante",
+  "Cheeky":"atrevido",
+  "Conceited":"engreído",
+  "Depressed":"deprimido",
+  "Faith":"fe",
+  "Furnished":"amueblado",
+  "Gang":"pandilla",
+  "Jealous":"celoso",
+  "Pensioner":"pensionista",
+  "Pretentious":"pretencioso",
+  "Priest":"sacerdote",
+  "Spoilt":"mimado",
+  "Spot":"grano",
+  "Stubborn":"terco",
+  "Underage":"menor de edad",
+  "":"",
+  "":"",
+  "Climbing":"escalada en roca",
+  "Body building":"culturismo",
+  "Folk music":"música folklórica",
+  "Good Friday":"Viernes Santo",
+  "Games console":"consola de juegos",
+  "Hobby":"pasatiempo",
+  "Nightclub":"club nocturno",
+  "Orchestra":"orquesta",
+  "Parachuting":"salto con paracaídas",
+  "Pocket money":"dinero de bolsillo",
+  "Referee":"árbitro",
+  "Saxophone":"saxófono",
+  "Soap opera":"telenovela",
+  "Trampolining":"cama elástica",
+  "":"",
+  "Archery":"tiro con arco",
+  "Board game":"juego de mesa",
+  "Cable":"televisión por cable",
+  "DIY":"bricolaje",
+  "Earphones":"auriculares",
+  "Engagement":"compromiso",
+  "Fencing":"esgrima",
+  "Fishing rod":"caña",
+  "Melody":"melodía",
+  "Rowing":"remo",
+  "Sailing boat":"barco",
+  "Scuba diving":"submarinismo",
+  "Subtitles":"subtítulos",
+  "Viewer":"audiencia(f)",
+  "":"",
+  "Art gallery":"galería de arte",
+  "Bed linen":"ropa de cama",
+  "B&B":"pensión",
+  "":"",
+  "Caravan":"caravana",
+  "Train carriage":"vagón",
+  "Diversion":"desvío",
+  "Rubbish bin":"cubo de la basura",
+  "Hill":"colina",
+  "Ice":"pista de patinaje",
+  "Identity":"carné de identidad",
+  "In advance":"por adelantado",
+  "Moped":"ciclomotor",
+  "Nightlife":"vida nocturna",
+  "Night":"club nocturno",
+  "Pavement":"acera",
+  "Pillow":"almohada",
+  "Priority":"prioridad",
+  "Province":"provincia",
+  "Roundabout":"glorieta",
+  "Sheet":"sábana",
+  "Sleeping":"coche",
+  "Suburb":"suburbio",
+  "Summer":"colonia de verano",
+  "Ticket":"mostrador de billetes",
+  "Toothbrush":"cepillo de dientes",
+  "Tour":"gira",
+  "Shop":"escaparate",
+  "":"",
+  "Dry cleaners":"tintorería",
+  "Dry cleaning":"limpieza en seco",
+  "Event":"acontecimiento",
+  "Flea market":"rastro",
+  "Helicopter":"helicóptero",
+  "Level crossing":"pasaje a nivel",
+  "Package holiday":"viaje organizado",
+  "Parade":"desfile",
+  "Registration":"inscripción",
+  "Toll":"peaje",
+  "":"",
+  "":"",
+  "Dry":"seco",
+  "cloud":"nube",
+  "Customer service":"información",
+  "Delivery":"entrega",
+  "Email address":"correo electrónico",
+  "Form":"formulario",
+  "Guarantee":"garantía",
+  "Receipt":"recibo",
+  "Replacement":"piezas de recambio",
+  "Waiting":"período de espera",
+  "Wallet":"billetero",
+  "Crash":"choque",
+  "Wrong number":"número equivocado",
+  "":"",
+  "Achievement":"éxito",
+  "Answer":"respuesta",
+  "":"",
+  "Canteen":"cantina",
+  "Caretaker":"conserje",
+  "Changing room":"vestuario",
+  "Exam paper":"hoja de examen",
+  "Corridor":"corredor",
+  "Desk":"pupitre",
+  "Detention":"castigo",
+  "Dictionary":"diccionario",
+  "Exercise":"ejercicio",
+  "Felt tip":"rotulador",
+  "Fountain pen":"pluma",
+  "Glue":"goma de pegar",
+  "Page":"página",
+  "":"",
+  "Projector":"proyector",
+  "":"",
+  "School trip":"excursión del colegio",
+  "Subject":"asignatura",
+  "Summer holidays":"vacaciones del verano",
+  "":"",
+  "Boarding school":"internado",
+  "Answerphone":"contestador automático",
+  "Baker":"panadero",
+  "Busy":"ocupado",
+  "Civil servant":"funcionario",
+  "Dentist":"dentista",
+  "Farmer":"agricultor",
+  "Farm worker":"trabajador",
+  "File":"expediente",
+  "Marketing":"mercadotecnia",
+  "Mechanic":"mecánico",
+  "Musician":"músico",
+  "Nurse":"enfermero",
+  "Pharmacist":"farmacéutico",
+  "Planned":"planificado",
+  "Plumber":"fontanero",
+  "Poet":"poeta",
+  "Police officer":"agente de policía",
+  "Programmer":"programador",
+  "Salesperson":"representante de ventas",
+  "Salary":"salario",
+  "Sewing":"costura",
+  "":"",
+  "Fundraiser":"venta benéfica",
+  "Database":"base de datos",
+  "Enclosed":"adjunto",
+  "Internship":"internado",
+  "vacancy":"anuncio de trabajo",
+  "Qualification":"título",
+  "Foundation":"",
+  "Disaster":"desastre",
+  "Flood":"inundaciónes",
+  "Rainforest":"selva tropical",
+  "War":"guerra",
+  "":"",
+  "Earthquake":"terremoto",
+  "Freshwater":"aqua dulce",
+  "Saltwater":"agua salada"
+}
 
 var elementsInsideBody = [...document.body.getElementsByTagName('*')];
 
-
 //Function to review DOM elements
-function findAndReplace(aqaSpanish){
+function findAndReplace(edexcelSpanish){
   elementsInsideBody.forEach(element =>{
     element.childNodes.forEach(child =>{
       if(child.nodeType === 3){
-        replaceText(child, aqaSpanish);
+        replaceText(child, edexcelSpanish);
         console.log('replaced');
       }
     });
   });
 }
 
-const aqaSpanish = {
-  "week": " semana",
-  "weekend": "fin de semana",
-  "and": 'y',
-  "month": "mes",
-  "sun": "sol",
-  "death": "muerte",
-  "deaths": "muertes"
-}
-
-function replaceText (node, aqaSpanish) {
+function replaceText (node, edexcelSpanish) {
   let value = node.nodeValue;
-  let aqaSpanishArray = Object.entries(aqaSpanish);
 
-    //For Loop Iterating over Array of Spanish-English Pairs
-    var i;
-    for (i = 0; i < aqaSpanishArray.length; i++) {
-      //Add a space either side of word so no unwanted overly aggressive matches
-      let space = " "
-      let englishWord = aqaSpanishArray[i][0];
-      let spacedEnglishWord = space.concat(englishWord, " ");
-      let spanishWord = aqaSpanishArray[i][0];
-      let spacedSpanishWord = space.concat(spanishWord, " ");
+  let spanish = Object.keys(edexcelSpanish);
+  let english = Object.values(edexcelSpanish);
 
-      //Regex passing in a variable instead of String
-      var change = "regex"
-      var regexFunction = new RegExp(change, "g")
+  for (var i = 0; i < spanish.length; i++) {
 
-          //If statement - If value matches spacedEnglishWord
-          //Wasn't working so added value definition locally to try
-          if (value == spacedEnglishWord) {
-          let value = node.nodeValue;
-          value = value.replace(regexFunction, spacedSpanishWord);
-          node.nodeValue = value;
-          }
-        }
-}
+      var spanishString = spanish[i]
+      var englishString = english[i]
+      var space = " "
+      var spacedSpanishString = space.concat(spanishString, " ");
+      var spacedEnglishString = space.concat(englishString, " ");
+      
+      //Regex using variables
+      var regexFunction = new RegExp(spacedSpanishString, "gi")
 
-window.onload = findAndReplace(aqaSpanish);
+      // New value = old value replaced
+      value = value.replace(regexFunction, spacedEnglishString);
+
+      //Return new value to the DOM
+      node.nodeValue = value;
+      //}
+    //}
+  }
+} 
+
+//When Window loads call the function. 
+window.onload = findAndReplace(edexcelSpanish);
+
+
 
 /*
-
 // Needs space before & after both values or replaces parts of words
 // Need to sort out case sensitivity...
 function replaceText (node) {
